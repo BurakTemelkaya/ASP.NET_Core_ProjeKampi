f3ec21f7b81e91c38953735ba60e75d3f24ec523
Burak Temelkaya <temelkayaburak@gmail.com>
2026-02-23 08:19:00 +0300

    Türkçe karekter sorunu düzeltildi.

--- CoreLayer/Extensions/ExceptionMiddleware.cs
+++ CoreLayer/Extensions/ExceptionMiddleware.cs
@@ -2,81 +2,77 @@
 using CoreLayer.CrossCuttingConcerns.Logging.Log4Net.Loggers;
 using CoreLayer.Extensions;
 using FluentValidation;
-using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 
-namespace Core.Extensions
+namespace Core.Extensions;
+
+public class ExceptionMiddleware
 {
-    public class ExceptionMiddleware
+    private RequestDelegate _next;
+
+    private LoggerServiceBase _databaseLoggerServiceBase;
+
+    private LoggerServiceBase _fileLoggerServiceBase;
+
+    public ExceptionMiddleware(RequestDelegate next)
     {
-        private RequestDelegate _next;
+        _next = next;
 
-        private LoggerServiceBase _databaseLoggerServiceBase;
+        _databaseLoggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(typeof(DatabaseLogger));
 
-        private LoggerServiceBase _fileLoggerServiceBase;
+        _fileLoggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(typeof(FileLogger));
+    }
 
-        public ExceptionMiddleware(RequestDelegate next)
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
         {
-            _next = next;
-
-            _databaseLoggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(typeof(DatabaseLogger));
-
-            _fileLoggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(typeof(FileLogger));
+            await _next(httpContext);
         }
-
-        public async Task InvokeAsync(HttpContext httpContext)
+        catch (OperationCanceledException)
         {
-            try
-            {
-                await _next(httpContext);
-            }
-            catch (OperationCanceledException)
-            {
-                // 1. İptal durumunda log kirliliği yapmıyoruz. 
-                _fileLoggerServiceBase.Info($"İşlem kullanıcı tarafından iptal edildi: {httpContext.Request.Path}");
+            // 1. İptal durumunda log kirliliği yapmıyoruz. 
+            _fileLoggerServiceBase.Info($"İşlem kullanıcı tarafından iptal edildi: {httpContext.Request.Path}");
 
-                // Bağlantı koptuğu için Redirect veya ağır modellerle uğraşmıyoruz.
-                httpContext.Response.StatusCode = 499;
-            }
-            catch (ValidationException validationException)
-            {
-                HandleValidationException(validationException, httpContext);
-            }
-            catch (Exception exception)
-            {
-                HandleGenericException(exception, httpContext);
-            }
+            // Bağlantı koptuğu için Redirect veya ağır modellerle uğraşmıyoruz.
+            httpContext.Response.StatusCode = 499;
         }
-
-        private void HandleValidationException(ValidationException exception, HttpContext httpContext)
+        catch (ValidationException validationException)
         {
-            ValidationErrorDetail exceptionModel = new()
-            {
-                StatusCode = (int)HttpStatusCode.BadRequest,
-                Message = exception.Message,
-                ValidationErrors = exception.Errors
-            };
-
-            _databaseLoggerServiceBase.Error(exceptionModel);
-            _fileLoggerServiceBase.Error(exceptionModel);
-
-            // Header gönderilmediyse yönlendir
-            if (!httpContext.Response.HasStarted)
-                httpContext.Response.Redirect(httpContext.Request.Path.ToString());
+            HandleValidationException(validationException, httpContext);
         }
-
-        private void HandleGenericException(Exception exception, HttpContext httpContext)
+        catch (Exception exception)
         {
-            _databaseLoggerServiceBase.Error(exception);
-            _fileLoggerServiceBase.Error(exception);
-
-            if (!httpContext.Response.HasStarted)
-                httpContext.Response.Redirect("/ErrorPage/Error404");
+            HandleGenericException(exception, httpContext);
         }
+    }
+
+    private void HandleValidationException(ValidationException exception, HttpContext httpContext)
+    {
+        ValidationErrorDetail exceptionModel = new()
+        {
+            StatusCode = (int)HttpStatusCode.BadRequest,
+            Message = exception.Message,
+            ValidationErrors = exception.Errors
+        };
+
+        _databaseLoggerServiceBase.Error(exceptionModel);
+        _fileLoggerServiceBase.Error(exceptionModel);
+
+        // Header gönderilmediyse yönlendir
+        if (!httpContext.Response.HasStarted)
+            httpContext.Response.Redirect(httpContext.Request.Path.ToString());
+    }
+
+    private void HandleGenericException(Exception exception, HttpContext httpContext)
+    {
+        _databaseLoggerServiceBase.Error(exception);
+        _fileLoggerServiceBase.Error(exception);
+
+        if (!httpContext.Response.HasStarted)
+            httpContext.Response.Redirect("/ErrorPage/Error404");
     }
 }
